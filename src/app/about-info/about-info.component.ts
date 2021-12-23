39d13d14e1a64cc7c8864c261c0abf1af445bd63
guozhao <davidkoojohn@163.com>
2021-12-23 21:44:38 +0800

    s

--- src/app/about-info/about-info.component.ts
+++ src/app/about-info/about-info.component.ts
@@ -8,8 +8,30 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 export class AboutInfoComponent implements OnInit {
 
   constructor() { }
+  isBlue: boolean = true
+  styleObj = {
+    color: "blue",
+    height: "100px",
+    lineHeight: "100px",
+    border: "1px dashed #333",
+    marginBottom: "10px"
+  }
+  val = "default val"
 
   ngOnInit(): void {
   }
 
+  handleChange(value: string) {
+    this.val = value
+  }
+
+  handleClick(event: any) {
+    console.log(event.path)
+  }
+
+  handleSubmit(ele: any, event: any) {
+    event.preventDefault()
+    console.log(ele)
+    // return false
+  }
 }
